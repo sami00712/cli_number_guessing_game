9a813d911ae9b86ab52018c9e06d70554153bf09
unknown <samiqaimkhani43@gmail.com>
2024-04-03 22:51:30 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 1 + 10);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number 1 to 10:",
+    }
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulation ! You guessed a right number.");
+}
+else {
+    console.log("You guessed a wrong number ");
+}
